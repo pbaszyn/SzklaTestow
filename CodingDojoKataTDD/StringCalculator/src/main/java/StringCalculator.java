34198e63dd013f127b56b7cad39d8483d203e6fd
pbaszyn <61763299+pbaszyn@users.noreply.github.com>
2020-08-19 20:10:39 +0200

    StringCalculator - refactor, new class for string operation added

--- CodingDojoKataTDD/StringCalculator/src/main/java/StringCalculator.java
+++ CodingDojoKataTDD/StringCalculator/src/main/java/StringCalculator.java
@@ -1,75 +1,22 @@
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class StringCalculator {
 
     public String add(String numbers) {
-        double result = 0.0;
-        int stringNumbersIndex;
 
-        if (numbers.isBlank()) {
+        StringOfNumbersManipulator stringOfNumbers = new StringOfNumbersManipulator(numbers);
+        stringOfNumbers.analyzeString();
+
+        if (stringOfNumbers.isEmpty()) {
             return "0.0";
-        }
-
-
-        stringNumbersIndex = illegalSeparatorFound(",\\n", numbers);
-        if (stringNumbersIndex > -1){
-            return "Number expected but '\\n' found at position " + (stringNumbersIndex+1) + ".";
-
-        }
-
-        stringNumbersIndex = illegalSeparatorFound("\\n,", numbers);
-        if (stringNumbersIndex > -1){
-            return "Number expected but ',' found at position " + (stringNumbersIndex+1) + ".";
-
-        }
-
-        stringNumbersIndex = illegalSeparatorFound(",$", numbers);
-        if (stringNumbersIndex > -1){
+        }else if (stringOfNumbers.isIllegalSeparator()){
+            return "Number expected but '" + stringOfNumbers.getIllegalSeparator() + "' found at position " + stringOfNumbers.getIllegalSeparatorPosition() + ".";
+        }else if (stringOfNumbers.eofFoundInsteadOfNumber()){
             return "Number expected but EOF found";
-
+        }else if (stringOfNumbers.foundNegativeNumbers()){
+            return "Negative not allowed :" + stringOfNumbers.getNegativeNumbersList();
         }
 
-        String NegativeNumbersList = isNegativeNumbers(numbers);
-        if (!NegativeNumbersList.isBlank()){
-            return "Negative not allowed :" + NegativeNumbersList;
-        }
+            return stringOfNumbers.getSum();
 
-
-        Pattern p = Pattern.compile("\\d+\\.\\d+|\\d+");
-        Matcher m = p.matcher(numbers);
-        while (m.find()){
-            result += Double.parseDouble(m.group());
-            System.out.println(m.group());
-        }
-        return result + "";
     }
 
-    private int illegalSeparatorFound(String pattern, String numbers){
-
-        Pattern p = Pattern.compile(pattern);
-        Matcher m = p.matcher(numbers);
-        if (m.find()){
-            return m.start();
-        }
-        return -1;
-    }
-
-    private String isNegativeNumbers(String numbers){
-        StringBuilder NegativeNumbersList = new StringBuilder();
-        Pattern p = Pattern.compile("-\\d+\\.\\d+|-\\d+");
-        Matcher m = p.matcher(numbers);
-        boolean first = true;
-        while (m.find()){
-            if (first){
-                NegativeNumbersList.append(" ").append(m.group());
-                first = false;
-            }else {
-                NegativeNumbersList.append(", ").append(m.group());
-            }
-
-
-        }
-        return NegativeNumbersList.toString();
-    }
 }
---
+++ CodingDojoKataTDD/StringCalculator/src/main/java/StringOfNumbersManipulator.java
@@ -0,0 +1,86 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringOfNumbersManipulator {
+
+    private final String numbers;
+    private String illegalSeparator = "";
+    private int illegalSeparatorPosition = -1;
+    private boolean eofFoundInsteadOfNumber = false;
+    private String negativeNumbersList = "";
+
+    public String getNegativeNumbersList() {
+        return negativeNumbersList;
+    }
+    public boolean foundNegativeNumbers() {
+        return !negativeNumbersList.isEmpty();
+    }
+    public StringOfNumbersManipulator(String numbers) {
+        this.numbers = numbers;
+    }
+    public String getIllegalSeparator() {
+        return illegalSeparator;
+    }
+    public int getIllegalSeparatorPosition() { return illegalSeparatorPosition; }
+    public boolean isEmpty(){
+        return numbers.isBlank();
+    }
+    public boolean isIllegalSeparator(){
+        return illegalSeparatorPosition > -1;
+    }
+    public boolean eofFoundInsteadOfNumber(){
+        return eofFoundInsteadOfNumber;
+    }
+
+    public void analyzeString(){
+
+        if (searchForIllegalSeparators(",\\n")){ illegalSeparator = "\\n";}
+        if (searchForIllegalSeparators("\\n,")){ illegalSeparator = ",";}
+        if (searchForMissingNumberAtTheEnd()){ eofFoundInsteadOfNumber = true;}
+        searchForNegativeNumbers();
+
+    }
+
+    public String getSum(){
+        Pattern p = Pattern.compile("\\d+\\.\\d+|\\d+");
+        Matcher m = p.matcher(numbers);
+        double result = 0.0;
+        while (m.find()){
+            result += Double.parseDouble(m.group());
+        }
+        return result + "";
+    }
+
+    private boolean searchForIllegalSeparators(String pattern){
+        Pattern p = Pattern.compile(pattern);
+        Matcher m = p.matcher(numbers);
+        if (m.find()){
+            illegalSeparatorPosition = m.start() +1;
+            return true;
+        }
+        return false;
+    }
+    private boolean searchForMissingNumberAtTheEnd(){
+        Pattern p = Pattern.compile(",$");
+        Matcher m = p.matcher(numbers);
+        return m.find();
+    }
+
+    private void searchForNegativeNumbers (){
+        StringBuilder negativeNumbers = new StringBuilder();
+        Pattern p = Pattern.compile("-\\d+\\.\\d+|-\\d+");
+        Matcher m = p.matcher(numbers);
+        boolean first = true;
+        while (m.find()){
+            if (first){
+                negativeNumbers.append(" ").append(m.group());
+                first = false;
+            }else {
+                negativeNumbers.append(", ").append(m.group());
+            }
+
+
+        }
+        negativeNumbersList = negativeNumbers.toString();
+    }
+}
